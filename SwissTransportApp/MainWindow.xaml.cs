0994e308bb71188d68fe4de105384d6d629bd784
hammerhc <hammerhc@users.noreply.github.com>
2020-03-10 16:00:53 +0100

    Verbindungen changed

--- SwissTransportApp/MainWindow.xaml.cs
+++ SwissTransportApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwissTransport;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,24 +24,10 @@ namespace SwissTransportApp
         List<Verbindungen> verbindungList = new List<Verbindungen>();
         List<Abfahrtstafel> abfahrtstafelList = new List<Abfahrtstafel>();
         List<Stationen> stationenList = new List<Stationen>();
+        private ITransport transportAPI;
         public MainWindow()
         {
             InitializeComponent();
-
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            verbindungList.Add(new Verbindungen { VerbindungId = 1, VerbindungAb = "Luzern, 12:00", VerbindungAn = "Sursee, 13:00", VerbindungDauer = "60 min", VerbindungGleisAb = "Gl.3", VerbindungGleisAn = "Gl.3" });
-            dataGridVerbindung.ItemsSource = verbindungList;
 
             abfahrtstafelList.Add(new Abfahrtstafel { AbfahrtstafelId = 1, AbfahrtstafelAb = "Luzern, 12:00", AbfahrtstafelAn = "Sursee, 13:00", AbfahrtstafelDauer = "60 min", AbfahrtstafelGleisAb = "Gl.3", AbfahrtstafelGleisAn = "Gl.3" });
             abfahrtstafelList.Add(new Abfahrtstafel { AbfahrtstafelId = 1, AbfahrtstafelAb = "Luzern, 12:00", AbfahrtstafelAn = "Sursee, 13:00", AbfahrtstafelDauer = "60 min", AbfahrtstafelGleisAb = "Gl.3", AbfahrtstafelGleisAn = "Gl.3" });
@@ -66,6 +53,65 @@ namespace SwissTransportApp
             stationenList.Add(new Stationen { StationenId = 1, StationenName = "Bahnhof Sursee", StationenOrt = "Sursee", StationenTyp = "Bahnhof", StationenEntfernung = "10km", StationenMapX = "34324345", StationenMapY = "3453134213", StationenMapURL = "www.google.ch/maps" });
             stationenList.Add(new Stationen { StationenId = 1, StationenName = "Bahnhof Sursee", StationenOrt = "Sursee", StationenTyp = "Bahnhof", StationenEntfernung = "10km", StationenMapX = "34324345", StationenMapY = "3453134213", StationenMapURL = "www.google.ch/maps" });
             dataGridStationen.ItemsSource = stationenList;
+        }
+
+        private void btnVerbindungSuchenClick(object sender, RoutedEventArgs e)
+        {
+            string verbindungenVon = "";
+            string verbindungenNach = "";
+            DateTime? verbindungenDatum = DateTime.Now;
+            dateVerbindungenDate.SelectedDate = verbindungenDatum;
+            string verbindungenZeit = DateTime.Now.ToString();
+            txtVerbindungenTime.Text = verbindungenZeit;
+
+            if (txtVerbindungenVon.Text.Length > 0)
+            {
+                verbindungenVon = txtVerbindungenVon.Text;
+            } 
+            else
+            {
+                showError("Von ist leer");
+            }
+            if (txtVerbindungenNach.Text.Length > 0)
+            {
+                verbindungenNach = txtVerbindungenNach.Text;
+            }
+            else
+            {
+                showError("Nach ist leer");
+            }
+            if (dateVerbindungenDate.SelectedDate >= DateTime.Now)
+            {
+                verbindungenDatum = dateVerbindungenDate.SelectedDate;
+            }
+            else
+            {
+                showError("Datum ist leer oder kleiner");
+            }
+            if (txtVerbindungenTime.Text.Length > 0)
+            {
+                verbindungenZeit = txtVerbindungenTime.Text;
+            } else
+            {
+                showError("Zeit ist leer");
+            }
+            bool? verbindungenAb = radioBtnVerbindungenAb.IsChecked;
+            bool? verbindungenAn = radioBtnVerbindungenAn.IsChecked;
+
+            transportAPI = new Transport();
+            var test = transportAPI.GetConnections(verbindungenVon, verbindungenNach);
+            int id = 0;
+            foreach (var line in test.ConnectionList)
+            {
+                verbindungList.Add(new Verbindungen { VerbindungId = id, VerbindungAb = line.From.Station.Name, VerbindungAn = line.To.Station.Name, VerbindungDauer = line.Duration, VerbindungGleisAb = line.From.Platform, VerbindungGleisAn = line.To.Platform });
+                id++;
+            }
+            dataGridVerbindung.ItemsSource = verbindungList;
+        }
+
+        private void showError(string message)
+        {
+            MessageBox.Show(message);
         }
     }
 }
